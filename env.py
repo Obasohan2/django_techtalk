9a35312bbe4fb88e509fc6ce90de9cdc0b62ad6d
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-02 16:11:20 +0100

    Added the Database, Secret Key and settings.py

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_4kvWB6lKufGY@ep-round-dawn-a29xp2ab.eu-central-1.aws.neon.tech/grill_try_rush_867260"
+os.environ["SECRET_KEY"] = "2k@wh)kf(p^l@&_yd-+a5ci^1(a20p_wszeey*!rl0@meuwqb^"
