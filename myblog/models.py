bc6a10956329092825a204885d87917b12b35991
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-02 21:50:21 +0100

    created database  models.py

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Community(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    caption = models.CharField(max_length=200, blank=True, null=True)
+    excerpt = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
