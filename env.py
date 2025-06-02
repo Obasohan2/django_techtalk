423a7d98f2227bb9dd4b994dfdd0c592c8c80486
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-02 16:31:30 +0100

    Create folders on the top directory level and added Procfile

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_4kvWB6lKufGY@ep-round-dawn-a29xp2ab.eu-central-1.aws.neon.tech/grill_try_rush_867260"
 os.environ["SECRET_KEY"] = "2k@wh)kf(p^l@&_yd-+a5ci^1(a20p_wszeey*!rl0@meuwqb^"
+os.environ["CLOUDINARY_URL"] = "cloudinary://557924331644286:dVIW2AW6TQKjTo6I4Stnl6hlzG0"
