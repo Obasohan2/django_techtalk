281610dbc7c411b879577c2cbed5d98455885893
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-02 17:10:37 +0100

    Deploy commit

--- techtalk/settings.py
+++ techtalk/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-techtalk-9412062b9dbd.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-techtalk-9412062b9dbd.herokuapp.com', '127.0.0.1', 'localhost:8000']
 
 
 # Application definition
